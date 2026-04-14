fd34c0a822950e49292a60d65b1f99c6528e686e
anisaskazi <nilskazi@gmail.com>
2026-04-14 22:23:54 +0530

    Add threeSum function in 3sum.cpp
    Implement threeSum function to find triplets that sum to zero.

---
+++ Day-13/3sum.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> result;
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    result.push_back({nums[i], nums[left], nums[right]});
+
+                    left++;
+                    right--;
+
+                    while (left < right && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+
+                    while (left < right && nums[right] == nums[right + 1]) {
+                        right--;
+                    }
+                }
+                else if (sum < 0) {
+                    left++;
+                }
+                else {
+                    right--;
+                }
+            }
+        }
+
+        return result;
+    }
+};
+
+int main() {
+    Solution obj;
+    vector<int> nums = {-1, 0, 1, 2, -1, -4};
+
+    vector<vector<int>> ans = obj.threeSum(nums);
+
+    for (auto &triplet : ans) {
+        cout << "[ ";
+        for (int x : triplet) {
+            cout << x << " ";
+        }
+        cout << "]" << endl;
+    }
+
+    return 0;
+}
